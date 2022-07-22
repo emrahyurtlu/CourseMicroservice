85f0857b94f74290a69ec976cab2274e91193cf1
Emrah YURTLU <emrahyurtlu@hotmail.com>
2022-07-22 08:07:47 +0300

    Dtos are added. Shared classlib is added.

---
+++ Services/Catalog/Course.Services.Catalog/Models/Feature.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Course.Services.Catalog.Models
+{
+    public class Feature
+    {
+        [BsonRepresentation(MongoDB.Bson.BsonType.Int32)]
+        public int Duration { get; set; }
+    }
+}
