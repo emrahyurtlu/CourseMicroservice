7b12f1dba86ba5d28954df4528ccafb7c4fdee7a
Emrah Yurtlu <emrahy@engineerica.com>
2023-09-09 23:33:20 +0300

    Implemented resources and persmissions in configs file.

---
+++ IdentityServer/Course.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Course.IdentityServer.Dtos;
+using Course.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Course.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City,
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest();
+            }
+
+            return Ok(result);
+        }
+    }
+}
