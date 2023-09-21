ca8f6be3ac486118dfb2062c863a9ae72b214acb
Emrah Yurtlu <emrahy@engineerica.com>
2023-09-21 23:27:05 +0300

    Worked on Order domain. Added some objects and dtos. Implemented automapper.

---
+++ Services/Order/Courses.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Courses.Services.Order.Application.Dtos;
+
+namespace Courses.Services.Order.Application.Mapping
+{
+    public class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<Domain.OrderAggregate.OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Domain.OrderAggregate.Address, AddressDto>().ReverseMap();
+        }
+    }
+}
---
+++ Services/Order/Courses.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace Courses.Services.Order.Application.Mapping
+{
+    public static class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> layz = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomMapping>();
+            });
+
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => layz.Value;
+    }
+}
